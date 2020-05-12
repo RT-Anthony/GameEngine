9202b43b3ffdd123b7da4500cc6b8ce7fd74713c
RT-Anthony <anthony.bell@roosterteeth.com>
2020-05-12 16:23:52 -0500

    beginning work on layers

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -49,16 +49,14 @@ namespace Hazel {
 
 	class HAZEL_API Event
 	{
-		friend class EventDispatcher;
 	public:
+		bool Handled = false;
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
 		virtual std::string ToString() const { return GetName(); }
 
 		inline bool IsInCategory(EventCategory category){ return GetCategoryFlags() & category; }
-	protected:
-		bool m_Handled = false;
 	}; // Event
 
 	class EventDispatcher
@@ -73,7 +71,7 @@ namespace Hazel {
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.m_Handled = func(*(T*)&m_Event);
+				m_Event.Handled = func(*(T*)&m_Event);
 				return true;
 			}
 			return false;
